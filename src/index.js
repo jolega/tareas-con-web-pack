5f21eba6f61ddb00b67cd272ff166dc130a914c7
jolega <alonso_juancho@hotmail.com>
2022-03-15 07:01:33 -0500

    cargar el localstorage

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import {Todo, TodoList, crearTodoHtml} from './classes';
 
 export const todoList= new TodoList();
 
-const tarea = new Todo('aprender JavaScript');
-
-
-
-todoList.nuevoTodo( tarea);
-
-crearTodoHtml( tarea );
-
-localStorage.setItem('mi-key','abc123');
-
-// setTimeout(()=> {
-//     localStorage.removeItem('mi-key')
-// },1500)
+todoList.todos.forEach( todo =>  crearTodoHtml(todo ));
 
 
